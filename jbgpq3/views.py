1f952d4934171d3e10d21bd7cb11544c86c77ccc
Andy Davidson <andy@nosignal.org>
2017-07-05 16:19:49 +0100

    inital work to wrap bgpq3 command.

--- jbgpq3/views.py
+++ jbgpq3/views.py
@@ -1,6 +1,17 @@
 from flask import Flask, request, abort, jsonify
 from jbgpq3 import app
 
+import subprocess
+import json
+import re
+
 @app.route('/')
 def index():
     return "fartypants"
+
+@app.route('/prefix/<as_set>')
+def get_prefix(as_set):
+    if not re.match('^AS', as_set):
+        abort(403, "Doesn't look like an AS-SET or aut-num - otherwise it would begin 'AS'.  ")
+    raw_prefixlist = json.loads(subprocess.check_output(['bgpq3', '-Aj', as_set]))
+    return jsonify(raw_prefixlist)
