fe875a1555bfecf274bc02f2f2bfa45bea695dfe
Andy Davidson <andy@nosignal.org>
2017-07-05 17:02:23 +0100

    add ipv6 prefix dump

--- jbgpq3/views.py
+++ jbgpq3/views.py
@@ -29,9 +29,26 @@ def get_prefix(as_set):
     else:
         display_prefixlist["cache_hit"] = False
         display_prefixlist["cache_age_seconds"] = 0
-        raw_prefixlist = subprocess.check_output(['bgpq3', '-Aj', as_set])
+        raw_prefixlist = subprocess.check_output(['bgpq3', '-A4j', as_set])
         with open("/tmp/jbgpq3_4" + as_set, "w") as myfile:
             myfile.write(raw_prefixlist)    
     display_prefixlist["prefixes_ipv4"] = json.loads(raw_prefixlist)["NN"]
+
+
+    # IPv6 prefixes
+    if os.path.isfile("/tmp/jbgpq3_6" + as_set):
+        display_prefixlist["cache_hit"] = True
+        filestats = os.stat("/tmp/jbgpq3_6" + as_set)
+        display_prefixlist["cache_age_seconds"] = int(time.time() - filestats.st_mtime)
+        with open("/tmp/jbgpq3_6" + as_set, "r") as myfile:
+            raw_prefixlist = ''.join(myfile.read().splitlines())
+    else:
+        display_prefixlist["cache_hit"] = False
+        display_prefixlist["cache_age_seconds"] = 0
+        raw_prefixlist = subprocess.check_output(['bgpq3', '-A6j', as_set])
+        with open("/tmp/jbgpq3_6" + as_set, "w") as myfile:
+            myfile.write(raw_prefixlist)    
+    display_prefixlist["prefixes_ipv6"] = json.loads(raw_prefixlist)["NN"]
+
 
     return jsonify(display_prefixlist)
