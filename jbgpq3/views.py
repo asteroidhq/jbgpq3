187e5814ff8f01f01bf6e2e2cf326e4baeb90675
Andy Davidson <andy@nosignal.org>
2017-07-05 17:12:32 +0100

    refactor, display if the cache is stale and to be deleted

--- jbgpq3/views.py
+++ jbgpq3/views.py
@@ -7,6 +7,8 @@ import re
 import os
 import time
 
+CACHE_MAX_AGE_POLICY_SECONDS = 2700 # 45 minutes
+
 @app.route('/')
 def index():
     return "fartypants"
@@ -19,16 +21,33 @@ def get_prefix(as_set):
     display_prefixlist = {}
     display_prefixlist["as_set"] = as_set
 
-    # IPv4 prefixes
+    # Test to see if the cache is too old to use
     if os.path.isfile("/tmp/jbgpq3_4" + as_set):
-        display_prefixlist["cache_hit"] = True
         filestats = os.stat("/tmp/jbgpq3_4" + as_set)
-        display_prefixlist["cache_age_seconds"] = int(time.time() - filestats.st_mtime)
+        file_age = int(time.time() - filestats.st_mtime)
+
+        if file_age > CACHE_MAX_AGE_POLICY_SECONDS:
+            # Cache is stale, remove the files and fetch new
+            display_prefixlist["cache_deleted"] = True
+            os.remove("/tmp/jbgpq3_4" + as_set)
+            os.remove("/tmp/jbgpq3_6" + as_set)
+            display_prefixlist["cache_hit"] = False
+            display_prefixlist["cache_age_seconds"] = 0
+        else:
+            # Cache is not stale, use the cache
+            display_prefixlist["cache_hit"] = True
+            display_prefixlist["cache_age_seconds"] = file_age
+    else:
+        # Cache file does not exist, fetch
+        display_prefixlist["cache_hit"] = False
+        display_prefixlist["cache_age_seconds"] = 0
+
+
+    # IPv4 prefixes
+    if display_prefixlist["cache_hit"]:
         with open("/tmp/jbgpq3_4" + as_set, "r") as myfile:
             raw_prefixlist = ''.join(myfile.read().splitlines())
     else:
-        display_prefixlist["cache_hit"] = False
-        display_prefixlist["cache_age_seconds"] = 0
         raw_prefixlist = subprocess.check_output(['bgpq3', '-A4j', as_set])
         with open("/tmp/jbgpq3_4" + as_set, "w") as myfile:
             myfile.write(raw_prefixlist)    
@@ -36,15 +55,10 @@ def get_prefix(as_set):
 
 
     # IPv6 prefixes
-    if os.path.isfile("/tmp/jbgpq3_6" + as_set):
-        display_prefixlist["cache_hit"] = True
-        filestats = os.stat("/tmp/jbgpq3_6" + as_set)
-        display_prefixlist["cache_age_seconds"] = int(time.time() - filestats.st_mtime)
+    if display_prefixlist["cache_hit"]:
         with open("/tmp/jbgpq3_6" + as_set, "r") as myfile:
             raw_prefixlist = ''.join(myfile.read().splitlines())
     else:
-        display_prefixlist["cache_hit"] = False
-        display_prefixlist["cache_age_seconds"] = 0
         raw_prefixlist = subprocess.check_output(['bgpq3', '-A6j', as_set])
         with open("/tmp/jbgpq3_6" + as_set, "w") as myfile:
             myfile.write(raw_prefixlist)    
